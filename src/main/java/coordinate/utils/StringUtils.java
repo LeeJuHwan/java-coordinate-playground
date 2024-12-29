17f2f8bffbf768dabd576fe70bcc90b7680212c5
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-12-29 16:19:12 +0900

    feat: application string util class

---
+++ src/main/java/coordinate/utils/StringUtils.java
@@ -0,0 +1,43 @@
+package coordinate.utils;
+
+import coordinate.exception.CoordinateException;
+import coordinate.messages.ErrorMessage;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class StringUtils {
+
+    public static List<String> splitByHyphen(String string) {
+        if (!string.contains("-")) {
+            throw new CoordinateException(ErrorMessage.hyphenMustHave);
+        }
+
+        return List.of(string.split("-"));
+    }
+
+    public static List<Integer> extractFromStringByRegex(String string) {
+        String regex = "\\((\\d+),(\\d+)\\)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(string);
+
+        if (matcher.matches()) {
+            return IntStream.range(1, matcher.groupCount() + 1)
+                    .mapToObj(i -> stringMatcherToInt(matcher, i))
+                    .collect(Collectors.toList());
+        }
+
+        throw new CoordinateException(ErrorMessage.regexNotMatched);
+    }
+
+    private static int stringMatcherToInt(Matcher matcher, int groupIndex) {
+        try {
+            return Integer.parseInt(matcher.group(groupIndex));
+        } catch (NumberFormatException e) {
+            throw new CoordinateException(ErrorMessage.numberFormatFailed);
+        }
+    }
+
+}
