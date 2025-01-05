f15728820ef0b8ae6a09c7004bbe38652b625bdc
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2025-01-05 12:46:01 +0900

    update: delete the constant message

--- src/main/java/coordinate/messages/SystemMessage.java
+++ src/main/java/coordinate/messages/SystemMessage.java
@@ -3,6 +3,4 @@ package coordinate.messages;
 public class SystemMessage {
 
     public static final String inputFigureSize = "좌표를 입력하세요.";
-    public static final String distancePoints = "두 점 사이의 거리는: ";
-
 }
